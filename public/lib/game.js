a354d97a1a221adacddda9a92dfc6d7aa2abb367
Antho-CodeMaster <antho.samson@outlook.fr>
2024-12-11 16:55:04 -0500

    Refactor en utilisant des objets

---
+++ public/lib/game.js
@@ -0,0 +1,72 @@
+class Game {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+        this.players = {};
+        this.numPlayers = 0;
+        this.colliders = new Group();
+        this.ripples = new Ripples();
+    }
+
+    add(id, x, y, w, h) {
+        console.log("Adding player:", id, x, y, w, h);
+        const player = new Player(id, x, y, w, h, this.numPlayers);
+        this.players[id] = player;
+        this.colliders.add(player.sprite);
+        this.numPlayers++;
+        savePlayerData(player, id);
+    }
+
+    draw() {
+        this.checkBounds();
+        this.ripples.draw();
+        drawSprites();
+    }
+
+    remove(id) {
+        if (this.players[id]) {
+            this.players[id].remove();
+            delete this.players[id];
+            this.numPlayers--;
+        }
+    }
+
+    setColor(id, r, g, b) {
+        if (this.players[id]) {
+            this.players[id].setColor(r, g, b);
+        }
+    }
+
+    setVelocity(id, velX, velY) {
+        if (this.players[id]) {
+            this.players[id].setVelocity(velX, velY);
+        }
+    }
+
+    checkBounds() {
+        for (let id in this.players) {
+            this.players[id].checkBounds(this.w, this.h);
+        }
+    }
+
+    checkId(id) {
+        // Check if player exists in the game
+        return id in this.players;
+    }
+
+    printPlayerIds(x, y) {
+        push();
+        noStroke();
+        fill(255);
+        textSize(16);
+        text(`# players: ${this.numPlayers}`, x, y);
+        y += 16;
+
+        for (let id in this.players) {
+            fill(200);
+            text(this.players[id].id, x, y);
+            y += 16;
+        }
+        pop();
+    }
+}
---
+++ public/lib/player.js
@@ -0,0 +1,45 @@
+class Player {
+    constructor(id, x, y, w, h, index) {
+        this.id = id;
+        this.index = index; // Player's index for differentiating sprites
+        this.position = createVector(x, y);
+        this.width = w;
+        this.height = h;
+        this.color = color(255, 255, 255); // Default white color
+        this.hp = 100; // Default health
+        this.shield = 50; // Default shield value
+        this.velocity = createVector(0, 0);
+
+        // Determine sprite based on index
+        const spriteImages = [player2, player1];
+        this.sprite = createSprite(x, y, w, h);
+        this.sprite.addImage(spriteImages[index % spriteImages.length]);
+
+        // Set initial sprite attributes
+        this.sprite.scale = 0.5;
+        this.sprite.shapeColor = this.color;
+    }
+
+    setVelocity(velX, velY) {
+        this.velocity.set(velX, velY);
+        this.sprite.setVelocity(velX, velY);
+    }
+
+    setColor(r, g, b) {
+        this.color = color(r, g, b);
+        this.sprite.shapeColor = this.color; // Update sprite color
+    }
+
+    checkBounds(width, height) {
+        if (this.position.x < 0) this.position.x = 0;
+        if (this.position.y < 0) this.position.y = 0;
+        if (this.position.x > width) this.position.x = width;
+        if (this.position.y > height) this.position.y = height;
+
+        this.sprite.position.set(this.position.x, this.position.y);
+    }
+
+    remove() {
+        this.sprite.remove();
+    }
+}
